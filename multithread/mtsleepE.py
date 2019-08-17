70feecb5a24a25931afc5f2f4bcae3ffb8daa2c6
Ten <esistkalt@naver.com>
2019-08-17 21:04:28 +0200

    서브클래싱 스레드

---
+++ multithread/mtsleepE.py
@@ -0,0 +1,56 @@
+# thread 클래스 상속해서 하위 클래스 인스턴스 작성
+
+# 서브클래싱 > 인스턴스화 대신 상속해서 사용해괴...
+
+import threading
+from time import sleep, ctime
+
+loops = [4, 2]
+
+
+class MyThread(threading.Thread) :
+  def __init__(self, func, args, name='') :
+    threading.Thread.__init__(self, name=name)
+    self.func = func
+    self.args =args
+  
+  
+  def run(self) :
+    self.func(*self.args)
+  
+  
+
+def loop(nloop, nsec) :
+  print 'start loop', nloop, 'at', ctime()
+  
+  sleep(nsec)
+  
+  print 'loop', nloop, 'done at ', ctime()
+  
+
+
+def main() :
+
+  print 'starting at : ', ctime()
+  threads = []
+  nloops = range(len(loops))
+  
+  for i in nloops :
+    t = MyThread(loop, (i, loops[i]), loop.__name__)
+    threads.append(t)
+  
+  
+  for i in nloops :
+    threads[i].start()
+    
+  
+  
+  for i in nloops :
+    threads[i].join()
+    
+  print 'all Done at : ', ctime()
+
+
+if __name__ == '__main__' :
+  main()
+    
