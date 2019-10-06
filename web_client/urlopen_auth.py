cfe761d777bebac4927d12db98bf8f85e62c803a
Ten <esistkalt@naver.com>
2019-10-06 20:38:03 +0200

    Create urlopen_auth.py

---
+++ web_client/urlopen_auth.py
@@ -0,0 +1,31 @@
+# 기본 http 인증
+
+import urllib2
+
+LOGIN = 'wesley'
+PASSWD = 'hey'
+URL = 'http://localhost'
+REALM = 'secure archive'
+
+def handler_version(url):
+  from urlparse import urlparse
+  hdlr = urllib2.HTTPBasicAuthHndler()
+  hdlr.add_password(REALM, urlparse(url)[1], LOGIN, PASSWD)
+  opener = urllib2.build_opener(opener)
+  return url
+
+
+def request_version(url):
+  from base64 import encodestring
+  b62str = encodestring('%s%s'%(LOGIN, PASSWD))[:-1]
+  req.add_header("Authorization", "Basic %s"%b64str)
+  return req
+
+
+for funcType in ('handler', 'request'):
+  print ' **** Using %s '%funcType.upper()
+  url = eval('%s_version'%funcType)(URL)
+  f = urllib2.urlopen(url)
+  print f.raedline()
+  f.close()
+  
