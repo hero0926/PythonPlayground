c0053008dd493d160ce00364c5bc653398d48cdb
hero0926 <esistkalt@naver.com>
2019-10-20 21:12:37 +0200

    add namespace, html

--- PycharmProjects/poll/config/urls.py
+++ PycharmProjects/poll/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = 'poll'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('poll/', include('poll.urls')),
--- PycharmProjects/poll/poll/views.py
+++ PycharmProjects/poll/poll/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
 
 def index(request):
-    return HttpResponse("Hello world")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    return render(request, 'poll/index.html', context)
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.object.get(pk=question_id)
+    # except:
+    #     raise Http404("Question does not exist.")
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {'question':question})
+
+def results(request, question_id):
+    response = "You are looking at question %s"
+    return HttpResponse(response%question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are looking at question %s"%question_id)
