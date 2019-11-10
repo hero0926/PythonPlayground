3cd51724395cfece1518c5c8bf95f12489c40e2d
hero0926 <esistkalt@naver.com>
2019-11-10 11:42:50 +0100

    c/r

--- PycharmProjects/bookmark/bookmark/views.py
+++ PycharmProjects/bookmark/bookmark/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
 # Create your views here.
 
 from .models import Bookmark
 
 class BookmarkListView(ListView):
+    model = Bookmark
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name', 'description', 'url']
+    success_url = reverse_lazy('list')
+    template_name_suffix = '_create'
+
+class BookmarkDetailView(DetailView):
     model = Bookmark
