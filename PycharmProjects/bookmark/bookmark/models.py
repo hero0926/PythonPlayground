e4718002c3cbcc8f038d945bff419a3ddbc82a88
hero0926 <esistkalt@naver.com>
2019-11-10 11:10:00 +0100

    urls/models

--- PycharmProjects/bookmark/bookmark/models.py
+++ PycharmProjects/bookmark/bookmark/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Bookmark(models.Model):
+    site_name = models.CharField(max_length=100)
+    url = models.URLField('Site url')
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return "site name : "+self.site_name+", url : "+self.url
