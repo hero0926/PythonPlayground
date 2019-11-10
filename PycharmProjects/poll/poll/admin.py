d52eb9ceea1307c4b7044f16348ed3e6fc8c07b9
hero0926 <esistkalt@naver.com>
2019-11-10 10:56:37 +0100

    poll example done

--- PycharmProjects/poll/poll/admin.py
+++ PycharmProjects/poll/poll/admin.py
@@ -1,6 +1,29 @@
+from datetime import timezone, datetime
+
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+#admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra =3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        (
+            'Date information', {'fields': ['pub_date']}
+        ),
+    ]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
